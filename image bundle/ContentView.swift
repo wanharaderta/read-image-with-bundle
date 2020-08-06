45c62282b00f205d7c0d6a495c6208c1ea133c8f
wanhar <wanhar@sehatq.com>
2020-08-06 12:34:28 +0700

    Initial Commit

---
+++ image bundle/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  image bundle
+//
+//  Created by Wanhar on 06/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ image bundle/image_bundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_bundleApp.swift
+//  image bundle
+//
+//  Created by Wanhar on 06/08/20.
+//
+
+import SwiftUI
+
+@main
+struct image_bundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
