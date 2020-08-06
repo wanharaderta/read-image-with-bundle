1e1415d3647b1c1ca31b92ea75abb0800ec40075
wanhar <wanhar@sehatq.com>
2020-08-06 13:55:09 +0700

    master

--- image bundle/ContentView.swift
+++ image bundle/ContentView.swift
@@ -6,10 +6,35 @@
 //
 
 import SwiftUI
+import SwiftSVG
+
 
 struct ContentView: View {
+    
+    func getPath(type:String) -> String {
+        let bundlePath = Bundle.main.path(forResource: "gambar", ofType: type)
+        print("path ->> \(bundlePath)")
+        return bundlePath ?? ""
+    }
+    
+    func getURL() -> URL {
+        let bundlePath = Bundle.main.url(forResource: "gambar", withExtension: "svg")
+        print("url ->> \(bundlePath)")
+        return bundlePath!
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack{
+            Image(uiImage: UIImage(contentsOfFile: getPath(type: "jpg"))!)
+                .resizable()
+                .frame(width: 100, height: 100)
+            Text("Image.JPG")
+            Divider()
+            SVGImage(url: getURL())
+                .frame(width: 100, height: 100)
+            Text("Image.SVG")
+        }
     }
 }
 
